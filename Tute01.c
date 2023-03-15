9ada50f186c885aae6a412f31a455ed226bdb1ef
Ranawaka2001 <127458462+Ranawaka2001@users.noreply.github.com>
2023-03-16 05:24:08 +0530

    Add Comments

--- Tute01.c
+++ Tute01.c
@@ -8,15 +8,15 @@ int main() {
   int mark1, mark2;
   float avg;
 
-  printf("Enter Subject 01 Mark : ");
+  printf("Enter Subject 01 Mark : ");     //Input subject 1 mark 
   scanf("%d", &mark1);
 
-  printf("Enter Subject 02 Mark : ");
+  printf("Enter Subject 02 Mark : ");     //Input subject 1 mark 
   scanf("%d", &mark2);
 
-  avg = (mark1 + mark2) / 2;
+  avg = (mark1 + mark2) / 2;              //Calculate average
 
-  printf("Average is : %.2f", avg);
+  printf("Average is : %.2f", avg);       //Display average
 
   return 0;
 }

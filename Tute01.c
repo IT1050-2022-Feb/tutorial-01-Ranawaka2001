9ed8103df5a9bd2f322af9c8c97fc9529a1c2401
Ranawaka2001 <127458462+Ranawaka2001@users.noreply.github.com>
2023-03-16 05:17:36 +0530

    Tutorial 01 Exercise 01

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
-  
+  int mark1, mark2;
+  float avg;
+
+  printf("Enter Subject 01 Mark : ");
+  scanf("%d", &mark1);
+
+  printf("Enter Subject 02 Mark : ");
+  scanf("%d", &mark2);
+
+  avg = (mark1 + mark2) / 2;
+
+  printf("Average is : %.2f", avg);
+
   return 0;
 }
 
